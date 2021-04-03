4c115b9fbf30209a69ca120b5f66af402994ac9c
Gustavo Pedroni <gstvpedroni@gmail.com>
2021-04-03 18:09:18 -0300

    Adding risk_score engine on risk_profile resource

--- src/app/resources/risk_profile/risk_profile_resource.py
+++ src/app/resources/risk_profile/risk_profile_resource.py
@@ -1,18 +1,16 @@
 from fastapi import APIRouter
 
-from .schemas import response_schema, request_schema
+from app.data.schemas.risk_profile import personal_information as p_info
+from app.data.schemas.risk_score import score
+from app.engines.risk_score.risk_score_engine import RiskScoreEngine
 
 router = APIRouter()
 
 
-@router.post("/", status_code=201, response_model=response_schema.RiskProfileResponseSchema)
-async def post(body: request_schema.RiskProfileRequestSchema):
+@router.post("/", status_code=201, response_model=score.RiskFinalScoreSchema)
+async def post(user_data: p_info.PersonalInformationSchema):
 
-    fake_response = {
-        "auto": "regular",
-        "disability": "ineligible",
-        "home": "economic",
-        "life": "regular"
-    }
+    risk_score_engine = RiskScoreEngine(user_data=user_data)
+    risk_score = risk_score_engine.calculate()
 
-    return fake_response
+    return risk_score
